18de5dcf09508f2553075021e837f27a0593abc4
unknown <rinadwihapsari5@gmail.com>
2024-03-23 11:51:24 +0700

    update

--- Tugas2_GitHub.py
+++ Tugas2_GitHub.py
@@ -1,10 +1,17 @@
 def profile(Nama,NIM,Prodi,Hobi):
 	""" function coba keyword arguments """
 	print("Profile Mahasiswa UNJANI Yogyakarta")
-	nama = int(input("Nama :"))
-	nim = int(input("NIM :"))
-	prodi = int(input("Prodi :"))
-	hobi = int(input("Hobi :"))
+	nama = str(input("Nama :"))
+	nim = str(input("NIM :"))
+	prodi = str(input("Prodi :"))
+	hobi = str(input("Hobi :"))
+
+	for key, value in enumerate (data):
+		print("Mahasiswa Prodi {} UNJANI Yogyakarta".format(prodi))
+		print("Dengan nama {}".format(nama))
+		print("Mempunyai NIM {}".format(nim))
+		print("Memiliki hobi {}".format(hobi))
+	
 
 data = {'Nama':"Puji Winar Cahyo",'NIM':"17000040",'Prodi':"Teknik Informatika",'Hobi':"coding"}
 profile(**data)
