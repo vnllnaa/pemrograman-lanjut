4735292cc5ff7713db11f67367f96cfbef129858
Rina Dwi H <164183326+vnllnaa@users.noreply.github.com>
2024-04-14 13:14:21 +0700

    Add files via upload

---
+++ Soal2_UTS.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def sepeda(normal_parameter, *args):
+    """function coba arguments"""
+    print("Merek sepeda :{}".format(args))
+    for args in args:
+        print("Merek sepeda :{}".format(args))
+
+sepeda("Poligon","United","Wim Cycle","Giant")
